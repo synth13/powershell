b53bb27a1cef02c7685687ba1a7468d5388382a9
synth13 <37087281+synth13@users.noreply.github.com>
2018-03-09 13:16:58 -0600

    Add files via upload

---
+++ ActiveDirectory.ps1
@@ -0,0 +1,55 @@
+﻿#########################################
+#Global Variables
+#########################################
+$ggName =  "Global_Group_Name"
+
+#########################################
+#functions
+#########################################
+
+function Get-GGMembers ($ggName) {
+    <#
+        get-adgroupmember fields returned by command
+            distingusihedName - contains organizations and OUs
+            name - user's acutal name
+            objectClass - user or usergroup
+            objectGUID
+            SamAccountName - unique name ID
+            SID
+    #>
+
+    $ggMembers = get-adgroupmember $ggName | ? {$_.objectClass -eq "user"} | select -ExpandProperty SamAccountName
+
+    return $ggMembers
+}
+
+function Get-MembersProfileInfo ($ggMembers) {
+    <#
+        get-ADUser fields returned by command
+            DistinguishedName
+            Enabled
+            GivenName
+            ObjectClass
+            ObjectGUID
+            SamAccountName
+            SID
+            Surname
+            UserPrincipalName
+    #>    
+    
+    $memberProfileInfo = @()
+
+    foreach ($ggMember in $ggMembers) {
+
+    $memberProfileInfo += get-ADUser $ggMember | Select -ExpandProperty UserPrincipalName
+
+    }
+
+
+    return $memberProfileInfo
+}
+
+
+$ggMembers = Get-GGMembers $ggName
+
+$memberProfileInfo = Get-MembersProfileInfo $ggMembers
